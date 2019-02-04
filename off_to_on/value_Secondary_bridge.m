0e85d1e6add491aee005df531e1d706491a80294
PratipRana <ranap@vcu.edu>
2019-02-04 18:00:17 -0500

    off to on fit not acheived

--- off_to_on/value_Secondary_bridge.m
+++ off_to_on/value_Secondary_bridge.m
@@ -5,59 +5,35 @@
 perS=5;
 perE=40;
 dilu=5;
-tht=100;
-
-%parameter
+tht=600000;
 n=27;
-% on pathway rate constant
-aon=0.5;
-bon=0.025;
-con=0.55e6;
-don=1e-3;
 
+% on pathway rate constant
+aon=0.10;
+bon=0.035;
+con=0.35e7;
+don=1e-3;
 %off pathway rate constant
 x=100e-1;
-y =1e-2;
-z=40e-1;
-zz=1e-2; 
-r1=1e4;
-s1=1e-1;
-f1=1e1;
+y =1e-1;
+z=140e-1;
+zz=10e-1; 
+r1=1e6;
+s1=2e-1;
+f1=1e5;
 f2=5e-3;
 p1=5e3;
 p2=6e-1;
 
 %bridge rate constant
-swiF=5e-2;
+swiF=5e15;
 swiB=1e-3;
 
 %fatty acid concentration
 Ecrt=.07e3;
 E=0.05e3;
 A_1=0.25;
-K=1;
-
-%condition check for CMC
-Emin=Ecrt-Ecrt/2;
-Emax=Ecrt+Ecrt/2;
-if (E<= Emin) % Lower CMC
-    Eeff=0;
-    con=con*K; 
-    don=don*K;
-end
-
-if (E>Emin) && (E< Emax) % Near CMC
-%   Eeff= ((Ecrt-Emin)-abs(E-Ecrt))
-    Eeff=E-2*Ecrt/3;
-    Eeff=Eeff/60;
-    Eeff=0.11;
-end
-
-if(E>= Emax)   % Higher CMC
-    Eeff=(E-Ecrt)/120;
-    r1=0;
-    r2=0;
-end
+Eeff=0.50;
 
 % call ode 
 theta=[aon,bon,con,don,x,y,z,zz,r1,s1,f1,f2,p1,p2]; 
@@ -86,55 +62,71 @@ Y_val=[Y_val1;dilu.*Y_val2];
 %claculate signal
 signalON=Y_val(:,n)*0;
 signalOFF=Y_val(:,n)*0;
-
-% for i=2:11
-% signalON=signalON + Y_val(:,i)*i;
-% end
 signalON=signalON + Y_val(:,12)*tht;
 
-% for i=13:20
-% signalOFF=signalOFF + Y_val(:,i).*(i-9);
-% end
+for i=13:21
+    signalOFF=signalOFF+Y_val(:,i).*(i-9);
+end
 
-% for i=13:20
-% signalOFF=signalOFF + Y_val(:,i).*(i-9);
-% end
-
-signalOFF=signalOFF+ 18*Y_val(:,22)+36*Y_val(:,23)+54*Y_val(:,24)+72*Y_val(:,25)+18*Y_val(:,26);
+signalOFF=signalOFF+18*Y_val(:,22)+36*Y_val(:,23)+54*Y_val(:,24)+72*Y_val(:,25)+18*Y_val(:,26);
 signal=signalON+signalOFF;
-% signal=signal(perS+1:end);
 signal = (signal - min(signal))/(max(signal) - min(signal));
-%signal = (signal - signal(perS))/(max(signal) - min(perS));
-%signal(signal(:)<0)=0;
+
 
 %plot
-
-%plot(t_range(perS+1:end), signal, '-r', 'LineWidth',2)
 plot(t_range, signal, '-b', 'LineWidth',2)
 hold on
-
-% num=xlsread('On & Of Pathway transition data.xlsx');
-% X=num(1:224,[1,6]);
-% X(:,1)=X(:,1);
-% X(:,2)=X(:,2);
-% %X(X(:,2)>0.1,2)=0.09;
-% %X(:,2)= (X(:,2) - min(X(:,2)))/(max(X(:,2)) - min(X(:,2)));
-% X(:,2)=(X(:,2)-min(X(:,2)))/(max(X(:,2))-min(X(:,2)));
-% plot(X(:,1), X(:,2),'--*g')
-% xlabel('Time')
-% ylabel('Normalized ThT')
-
 num=xlsread('off_on_final.xlsx');
 plot(num(:,1), 1.1*(num(:,3)-min(num(:,3)))/(max(num(:,3))-min(num(:,3))),'gs',...
     'LineWidth',2,...
     'MarkerSize',10,...
     'MarkerEdgeColor','b',...
     'MarkerFaceColor',[0.5,0.5,0.5]);
-% plot(num(:,1), (num(:,7)-min(num(:,7)))/(max(num(:,7))-min(num(:,7))),'gs',...
-%     'LineWidth',2,...
-%     'MarkerSize',10,...
-%     'MarkerEdgeColor','g',...
-%     'MarkerFaceColor',[0.25,0.25,0.25]);
+
+
+
+perS=24;
+perE=40;
+dilu=5;
+theta=[aon,bon,con,don,x,y,z,zz,r1,s1,f1,f2,p1,p2]; 
+Y0=zeros(1,n); 
+Y0(1)=A_1;
+Y0(n)=Eeff;
+% Y0(12)=5e-6;
+t_range1=linspace(0,perS,perS+1); 
+[t_val,Y_val1]=ode23s(@lee_ode_Secondary_bridge,t_range1,Y0,[],n,theta);
+
+
+theta=[aon,bon,con,don,x,y,z,zz,r1,s1,f1,f2,p1,p2,swiF,swiB]; 
+Y0=Y_val1(end,:)';
+Y0=Y0/dilu;
+Y0(1)=Y0(1)+0.25;
+t_range2=linspace(0,perE-perS, perE-perS+1)
+[t_val,Y_val2]=ode23s(@lee_ode_Secondary_bridge_on,t_range2,Y0,[],n,theta);
+
+Y_val=[Y_val1;Y_val2];
+t_range2=t_range2+perS;
+t_range=[t_range1 t_range2];
+
+Y_val([1:1:perE],[1 4 12 13  21  26 27])
+
+Y_val=[Y_val1;dilu.*Y_val2];
+%claculate signal
+signalON=Y_val(:,n)*0;
+signalOFF=Y_val(:,n)*0;
+signalON=signalON + Y_val(:,12)*tht;
+
+signalOFF=signalOFF+ 18*Y_val(:,22)+36*Y_val(:,23)+54*Y_val(:,24)+72*Y_val(:,25)+18*Y_val(:,26);
+signal=signalON+signalOFF;
+signal = (signal - min(signal))/(max(signal) - min(signal));
+plot(t_range, signal, '-g', 'LineWidth',2)
+hold on
+
+plot(num(:,1), (num(:,7)-min(num(:,7)))/(max(num(:,7))-min(num(:,7))),'gs',...
+    'LineWidth',2,...
+    'MarkerSize',10,...
+    'MarkerEdgeColor','g',...
+    'MarkerFaceColor',[0.25,0.25,0.25]);
 
 %md=fitlm(signal(25:end),X([1:6:end],2))
 
